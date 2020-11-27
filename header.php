1268038e89895a5b68113fe6b0cdd23b20b94de2
Ashraf Kamarudin <ashrafk@runcloud.io>
2020-11-28 05:21:05 +0800

    optimize

--- header.php
+++ header.php
@@ -1,9 +1,9 @@
 <meta charset="UTF-8">
 <meta content="width=device-width, initial-scale=1.0" name="viewport">
 <title><?php echo get_bloginfo( 'name' ); ?></title>
-<link href="<?php echo get_bloginfo('template_directory'); ?>/assets/css/app.css" rel="stylesheet" type="text/css">
-<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" rel="stylesheet">
-<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
-<link href="https://unpkg.com/pattern.css" rel="stylesheet">
+<link href="<?php echo get_bloginfo('template_directory'); ?>/assets/css/app.css" rel="stylesheet" type="text/css" defer>
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" rel="preload" as="style" onload="this.rel='stylesheet'" defer>
+<link type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" rel="preload" as="style" onload="this.rel='stylesheet'"  defer/>
+<link href="https://unpkg.com/pattern.css" rel="preload" as="style" onload="this.rel='stylesheet'" defer>
 
 <?php wp_head();?>
