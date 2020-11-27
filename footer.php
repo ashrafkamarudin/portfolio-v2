f57875963ca5e287f532241f6dfd401b46275043
Ashraf Kamarudin <ashrafk@runcloud.io>
2020-11-28 06:27:12 +0800

    fix spacing

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<footer class="w-full grid justify-items-center bg-white p-5">
+<footer class="w-full grid justify-items-center p-5">
     <div class="text-sm font-medium text-gray-600">
         Powered by <a href="https://wordpress.com/" class="text-blue-600 font-bold"><i class="fab fa-wordpress"></i> WordPress</a>, Built with <a href="https://tailwindcss.com/" class="font-bold" style="color: #00d6f2;"><i class="fab fa-tailwind"></i> Tailwind CSS v2.0</a>.
     </div>
--- single.php
+++ single.php
@@ -5,7 +5,7 @@
 </head>
 <body>
     <?php get_template_part( 'components/navbar' ); ?>
-    <main class="mx-auto max-w-7xl md:p-4 py-8 sm:mt-8 sm:px-6 md:mt-12 lg:mt-16 lg:px-8 xl:mt-20 bg-white">
+    <main class="mx-auto max-w-7xl md:p-4 py-8 sm:mt-8 sm:px-6 md:mt-12 lg:mt-16 lg:px-8 xl:mt-20 bg-white mb-5">
         
     <?php
         if ( have_posts() ) : while ( have_posts() ) : the_post();
@@ -14,6 +14,6 @@
 	?>
     </main>
 
-    <?php get_footer(); ?>
+<?php get_footer(); ?>
 </body>
 </html>
