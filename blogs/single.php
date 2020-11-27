d43ba8a0c9618afc605b1a35bbefcdb1ca5e5238
Ashraf Kamarudin <ashrafk@runcloud.io>
2020-11-28 04:00:04 +0800

    fix spacing

--- blogs/single.php
+++ blogs/single.php
@@ -4,7 +4,7 @@
             <p class="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
                 <?php get_template_part( 'components/tags-category-badges' ); ?>
             </p>
-            <p class="mt-2 text-6xl leading-12 lg:leading-8 font-extrabold tracking-tight text-gray-900 sm:text-6xl">
+            <p class="mt-2 md:mt-5 text-6xl leading-12 lg:leading-8 font-extrabold tracking-tight text-gray-900 sm:text-6xl">
                 <?php the_title() ?>
             </p>
             <h2 class="mt-5 text-base text-pink-500 font-semibold tracking-wide uppercase leading-loose"><?php the_date(); ?> <span class="text-gray-300 text-5xl md:text-3xl font-light ml-1 mr-5 md:mr-1 float-left md:float-none" style="vertical-align:bottom;"> / </span> <a href="#" class="prose"><?php the_author(); ?></a></h2>
