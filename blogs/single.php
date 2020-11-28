6cc7fee52e793913d360cd6bd7e589f588b5d0fd
Ashraf Kamarudin <ashrafk@runcloud.io>
2020-11-28 17:49:29 +0800

    fix pading blog

--- blogs/single.php
+++ blogs/single.php
@@ -7,11 +7,14 @@
             <p class="mt-2 md:mt-5 text-6xl leading-12 lg:leading-8 font-extrabold tracking-tight text-gray-900 sm:text-6xl">
                 <?php the_title() ?>
             </p>
-            <h2 class="mt-5 text-base text-pink-500 font-semibold tracking-wide uppercase leading-loose"><?php the_date(); ?> <span class="text-gray-300 text-5xl md:text-3xl font-light ml-1 mr-5 md:mr-1 float-left md:float-none" style="vertical-align:bottom;"> / </span> <a href="#" class="prose"><?php the_author(); ?></a></h2>
+            <h2 class="mt-5 text-base text-pink-500 font-semibold tracking-wide uppercase leading-loose">
+                <?php the_date(); ?> 
+                <span class="text-gray-300 text-5xl md:text-3xl font-light ml-1 mr-5 md:mr-1 float-left md:float-none" style="vertical-align:bottom;"> / </span> 
+                <br class="md:hidden">
+                <a href="#" class="prose"><?php the_author(); ?></a></h2>
         </div>
     </div>
 </div>
-
 <div class="py-15 p-5 md:p-20 flex item-center">
     <div class="text-gray-600 font-light text-lg leading-loose text-left blog-content break-normal">
         <?php the_content(); ?>
