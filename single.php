dc5d6ba68b4b8f98e3408f8240ec3828215ef89c
Ashraf Kamarudin <ashrafk@runcloud.io>
2020-12-06 16:42:49 +0800

    fix

--- single.php
+++ single.php
@@ -5,7 +5,7 @@
 </head>
 <body>
     <?php get_template_part( 'components/navbar' ); ?>
-    <main class="max-w-6xl mt-20 py-8 lg:max-w-4xl bg-white mb-5 mx-auto">
+    <main class="max-w-6xl mt-20 py-8 md:max-w-4xl bg-white mb-5 mx-auto">
         
     <?php
         if ( have_posts() ) : while ( have_posts() ) : the_post();
